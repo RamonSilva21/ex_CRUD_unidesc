55455f6aa88e7deded8654d470a79ee125a6bf7e
RamonSilva21 <66040319+RamonSilva21@users.noreply.github.com>
2020-09-29 18:17:28 -0300

    Update AlunoContract.java

--- CRUD/src/br/edu/unidesc/interfaces/AlunoContract.java
+++ CRUD/src/br/edu/unidesc/interfaces/AlunoContract.java
@@ -4,10 +4,10 @@ import br.edu.unidesc.entidades.Aluno;
 
 public interface AlunoContract {
 	
-	Aluno criarAluno();		//C REATE
+	Aluno criarAluno();			//C REATE
 	Aluno searchAluno(String cpf);		//R EAD
 	void atualizarAluno(String cpf);	//U PDATE
-	void deleteALuno(String cpf);		//D ELETE
+	Boolean deleteALuno(String cpf);	//D ELETE
 	
 
 }
